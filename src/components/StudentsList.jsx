6ee221b418122c44576993c46f9f99bb16be0c8c
anfels <anfel.selmi15@gmail.com>
2021-03-24 17:23:39 +0100

    display selected student aafter hover

--- src/components/StudentsList.jsx
+++ src/components/StudentsList.jsx
@@ -3,38 +3,37 @@ import React, { useState } from "react";
 
 const StudentsList = (props) => {
     const [selectedStudent, setSelectedStudent] = useState("");
-    const [selectedList, setselectedList] = useState("");
+    const [selectedStudentItem, setSelectedStudentItem] = useState(null);
     return (
         <div>
-
             <p> la liste de tous les étudiants : {props.listStudentsWithStatus.length}</p>
-            {props.listStudentsWithStatus.length === 0 ? <p>liste vide </p> : props.listStudentsWithStatus.map((student) => <div style={{ display: 'flex', gap: "12px" }}>
-                <div onMouseEnter={() =>
-                    setselectedList
-                    (student.name)}
-                    onMouseLeave={() => setselectedList("")}>
+            <p>étudiant séléctionné: {selectedStudentItem?.name} {selectedStudentItem?.age} {selectedStudentItem?.ville}</p>
+            {props.listStudentsWithStatus.length === 0 ? <p>liste vide </p> : props.listStudentsWithStatus.map((student) => <>
+                <div style={{ display: 'flex', gap: "12px" }}
+                    onMouseEnter={() =>
+                        setSelectedStudentItem
+                            (student)}
+                    onMouseLeave={() => setSelectedStudentItem(null)}>
+                    <p>{student.name}</p>
+                    <p>{student.age}</p>
+                    <p> {student.ville}</p>
+                    <p>{student.moyenne}</p>
+                    <p>{student.adress}</p>
+                    <p>{student.specialty}</p>
+                    <p>{student.level}</p>
+                    <p>{student.status}</p>
 
-                <p>{student.name}</p>
-                <p>{student.age}</p>
-                <p> {student.ville}</p>{selectedList}</div>
-                <p>{student.moyenne}</p>
-                <p>{student.adress}</p>
-                <p>{student.specialty}</p>
-                <p>{student.level}</p>
-                <p>{student.status}</p>
-                
-                <button
-                    onClick={() => {
-                        { props.deleteStudent(student) }
-                    }}
-                    onMouseEnter={() => {
-                        setSelectedStudent(student.name)
-                    }}
-                    onMouseLeave={() => setSelectedStudent("")}
-                >
-                    {selectedStudent === student.name ? "cliquer pour supprimé" : "delete student"}</button>
-
-            </div>
+                    <button
+                        onClick={() => {
+                            { props.deleteStudent(student) }
+                        }}
+                        onMouseEnter={() => {
+                            setSelectedStudent(student.name)
+                        }}
+                        onMouseLeave={() => setSelectedStudent("")}
+                    >
+                        {selectedStudent === student.name ? "cliquer pour supprimé" : "delete student"}</button>
+                </div></>
             )
             }
 
