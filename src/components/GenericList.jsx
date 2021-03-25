eab9e342d65a8478280a5ef1145d44118cc2bb75
anfels <anfel.selmi15@gmail.com>
2021-03-25 14:03:57 +0100

    GenericButton

--- src/components/GenericList.jsx
+++ src/components/GenericList.jsx
@@ -1,14 +1,6 @@
 import React, { useState } from "react";
-
-<<<<<<< HEAD:src/components/GenericList.jsx
-
-
 const GenericList = (props) => {
-=======
-const StudentsSucceed = (props) => {
->>>>>>> 820732a... students:src/components/StudentSucceed.jsx
     const [isDisplayStudents, setIsDisplayStudents] = useState(false);
-
     return (
         <div>
             <button onClick={() => {
@@ -18,13 +10,12 @@ const StudentsSucceed = (props) => {
             {isDisplayStudents && <div>
                 <p>{props.label} {props.students.length}</p>
                 {props.students.length === 0 ? <p>liste vide </p> : props.students.map((student) => <div style={{ display: 'flex', gap: "12px" }}>
-                <p>{student.name}</p>
-            </div>
-            )}
+                    <p>{student.name}</p>
+                </div>
+                )}
             </div>
             }
         </div>
     )
 }
-
 export default GenericList;
