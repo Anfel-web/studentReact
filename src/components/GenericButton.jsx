0710fbe71cb359212248f1ad4f75394f04a22f8d
anfels <anfel.selmi15@gmail.com>
2021-03-28 18:41:22 +0100

    generic button

--- src/components/GenericButton.jsx
+++ src/components/GenericButton.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 const GenericButton = (props) => {
+  const setTitleOnClick = () => {
+    if (props.label === "add client") {
+      props.setTitle("add client button à été cliquer ")
+    } else {
+      props.setTitle("remove client button à été cliquer ")
+    }
+  }
+
   return (
-    <button >{props.label}</button>
+    <div>
+      <button
+        onClick={() => setTitleOnClick()}
+      >{props.label}</button>
+      <p>button add a été clicker</p>
+    </div>
   )
 }
 export default GenericButton;
